ed0d5f7463cff17ec6e28713d5c9fec7a4e658be
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-18 23:23:46 -0700

    Added password crypt and login post functionality.

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,18 @@
 let express = require('express');
 let router = express.Router();
-let UserModel=require('../models/User')
+let bcrypt=require('bcrypt');
+let UserModel=require('../models/User');
 
+router.post('/',(req,res,next)=>{
+    let email=req.body.email;
+    let password=req.body.password;
+
+    UserModel.findOne({email})
+    .then((result)=>{
+        bcrypt.compare(password, result.password)
+        .then((data)=>{res.json(data)})
+    })
+    .catch((err)=>{res.json(err)})
+})
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let bcrypt=require('bcrypt');
 let UserModel=require('../models/User')
 
 router.post('/', (req,res,next)=>{
@@ -7,11 +8,14 @@ router.post('/', (req,res,next)=>{
     let lastName=req.body.lastName
     let email=req.body.email;
     let password=req.body.password;
-    
-    let newUser=new UserModel({name, lastName, email, password});
-    newUser.save()
-    .then((data)=>{res.json(data)})
-    .catch((err)=>{res.json(err)})  
+
+    bcrypt.hash(password,10)
+    .then((hash)=>{
+        let newUser=new UserModel({name, lastName, email, password:hash});
+        newUser.save()
+        .then((data)=>{res.json(data)})
+        .catch((err)=>{res.json(err)})
+    }) 
 })
 
 module.exports = router;
