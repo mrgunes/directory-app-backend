b0819d4a129e5da984dd1aa7fbc0d9f4aee94513
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-19 19:52:16 -0700

    Added user_id and aggreate sturucture.

--- routes/createcontact.js
+++ routes/createcontact.js
@@ -3,10 +3,11 @@ let express=require('express');
 let router=express.Router();
 let ContactModel=require('../models/Contact');
 
-router.post('/',(req,res,next)=>{
+router.post('/:user_id',(req,res,next)=>{
+    let user_id=req.params.user_id
     let {nameLastName, email, phone, address, social, company, job, note}=req.body;
     let newContact=new ContactModel({
-        nameLastName, email, phone, address, social, company, job, note
+        user_id, nameLastName, email, phone, address, social, company, job, note
     })
     newContact.save()
     .then((data)=>{res.json(data)})
---
+++ routes/userpage.js
@@ -0,0 +1,19 @@
+let express=require('express');
+let router=express.Router();
+let mongoose=require('mongoose');
+
+let UserModel=require('../models/User')
+
+router.get('/:userId', (req,res,next)=>{
+    UserModel.aggregate([
+        {$match:{id:mongoose.Types.ObjectId(req.params.userId)}},
+        {$lookup:{
+            from:'contacts',
+            localField:'_id',
+            foreignField:'user_id',
+            as:'contacts'
+        }}
+    ])
+    .then((data)=>{res.json(data)})
+    .catch((err)=>{res.json(err)})
+})
